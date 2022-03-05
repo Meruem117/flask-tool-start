6ca6e89da18668f288c774cc30deedb5eb462d5e
Meruem117 <1572677319@qq.com>
2022-03-05 17:08:31 +0800

    update templates

--- src/main.py
+++ src/main.py
@@ -1,50 +1,19 @@
-from flask import Flask, escape, url_for, request, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "Hello Flask"
+def index_page():
+    return render_template('index.html')
 
 
-@app.route('/user/<string:username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % escape(username)
-
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % escape(subpath)
-
-
-with app.test_request_context():
-    url_for('static', filename='style.css')
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
-
-
-@app.route('/form', methods=['GET', 'POST'])
-def form():
+@app.route('/image', methods=['GET', 'POST'])
+def image_page():
     if request.method == 'GET':
-        return render_template('form.html')
-    if request.method == 'POST':
-        name = request.form.get('name')
-        password = request.form.get('password')
-        print(name, password)
-        return render_template('form.html')
+        return render_template('image.html')
+    else:
+        return 'Submit'
 
 
 if __name__ == '__main__':
