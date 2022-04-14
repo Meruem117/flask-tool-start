3edbcc91aafe4c1cf0f575778a552db925c5a53c
Meruem117 <1572677319@qq.com>
2022-04-14 23:27:33 +0800

    update doc

--- src/modules/doc.py
+++ src/modules/doc.py
@@ -10,16 +10,33 @@ def open_docx(dirname, filename):
 
 def handle_table(docx):
     table = docx.tables[0]
-    replace_text(table, 0, 2, '2022年3月13日15时59分')
+    replace_text(table, 0, 2, 0, 0, '2022年3月13日15时59分')
+    replace_text(table, 2, 2, 0, 0, '王守义')
+    replace_text(table, 2, 2, 0, 1, '北港')
     docx.save('../static/tmp/demo.docx')
 
 
-def replace_text(table, row, col, text):
-    run = table.cell(row, col).paragraphs[0].runs[0]
+def replace_text(table, row, col, para, r, text):
+    run = table.cell(row, col).paragraphs[para].runs[r]
     run.text = run.text.replace(run.text, text)
+
+
+def add_text(table, row, col, para, text):
+    run = table.cell(row, col).paragraphs[para]
+    run.text = run.text.replace(run.text, text)
+
+
+def map_doc_table(document):
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    for run in paragraph.runs:
+                        print(run.text)
 
 
 if __name__ == '__main__':
     docx_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)) + '\\static\\docx'
     doc = open_docx(docx_dir, 'doc1.docx')
-    handle_table(doc)
+    map_doc_table(doc)
+    # handle_table(doc)
