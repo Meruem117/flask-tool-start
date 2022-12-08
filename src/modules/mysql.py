77f8971e5373f076ae17d7de1ecdaea091a47ec3
Meruem117 <1572677319@qq.com>
2022-12-08 23:33:36 +0800

    update

--- src/modules/mysql.py
+++ src/modules/mysql.py
@@ -1,18 +1,46 @@
 import pymysql
 
 
+def show_database():
+    con = pymysql.connect(host="localhost", port=3306, user="root", password="abc123", charset="utf8", db="bili")
+    cursor = con.cursor(cursor=pymysql.cursors.DictCursor)
+
+    sql = "show databases"
+    cursor.execute(sql)
+    database_list = cursor.fetchall()
+
+    cursor.close()
+    con.close()
+
+    return database_list
+
+
+def show_tables(database_name):
+    con = pymysql.connect(host="localhost", port=3306, user="root", password="abc123", charset="utf8", db="bili")
+    cursor = con.cursor(cursor=pymysql.cursors.DictCursor)
+
+    sql = "show tables from %s"
+    cursor.execute(sql % database_name)
+    table_list = cursor.fetchall()
+
+    cursor.close()
+    con.close()
+
+    return table_list
+
+
 def show_table_columns(table_name: str):
     con = pymysql.connect(host="localhost", port=3306, user="root", password="abc123", charset="utf8", db="bili")
     cursor = con.cursor(cursor=pymysql.cursors.DictCursor)
 
     sql = "show full columns from %s"
     cursor.execute(sql % table_name)
-    columns = cursor.fetchall()
+    column_list = cursor.fetchall()
 
     cursor.close()
     con.close()
 
-    return columns
+    return column_list
 
 
 def select_table_data(table_name: str):
@@ -30,5 +58,7 @@ def select_table_data(table_name: str):
 
 
 if __name__ == '__main__':
-    describe_table("up")
+    show_database()
+    # show_tables("bili")
+    # show_table_columns("up")
     # select_table_data("up")
