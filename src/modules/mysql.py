f41ab8876ddb0ef20770a629d876f0048ef2e4df
Meruem117 <1572677319@qq.com>
2022-12-08 23:28:49 +0800

    update

--- src/modules/mysql.py
+++ src/modules/mysql.py
@@ -1,13 +1,27 @@
 import pymysql
 
 
+def show_table_columns(table_name: str):
+    con = pymysql.connect(host="localhost", port=3306, user="root", password="abc123", charset="utf8", db="bili")
+    cursor = con.cursor(cursor=pymysql.cursors.DictCursor)
+
+    sql = "show full columns from %s"
+    cursor.execute(sql % table_name)
+    columns = cursor.fetchall()
+
+    cursor.close()
+    con.close()
+
+    return columns
+
+
 def select_table_data(table_name: str):
     con = pymysql.connect(host="localhost", port=3306, user="root", password="abc123", charset="utf8", db="bili")
     cursor = con.cursor(cursor=pymysql.cursors.DictCursor)
 
     sql = "select * from %s"
     cursor.execute(sql % table_name)
-    data_list = cursor.fetchall()
+    data_list = cursor.execute()
 
     cursor.close()
     con.close()
@@ -16,4 +30,5 @@ def select_table_data(table_name: str):
 
 
 if __name__ == '__main__':
-    select_table_data("up")
+    describe_table("up")
+    # select_table_data("up")
